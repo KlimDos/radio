0fa8c2563d980840b9969107c93469711e3c785c
Sasha Alimov <aalimov@wiley.com>
2022-12-16 17:30:04 -0500

    login form

--- src/app.py
+++ src/app.py
@@ -2,10 +2,18 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+app.config["DEBUG"] = True
+app.config['SECRET_KEY'] = 'secret-key'
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
 
 
 if __name__ == "__main__":
---
+++ src/config.py
@@ -0,0 +1,2 @@
+class Config(object):
+    SECRET_KEY = 'my-secrete-key'
