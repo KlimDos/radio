60e58c07984f8c8ee3691acfaf67f9e08e187009
Sasha Alimov <aalimov@wiley.com>
2022-12-29 16:30:49 -0500

    add ya.metric

--- src/static/gtag.js
+++ src/static/gtag.js
@@ -1,12 +1,8 @@
 // Google tag
 
-  async 
-  src="https://www.googletagmanager.com/gtag/js?id=G-7DJWRXDHYQ"
+window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-  window.dataLayer = window.dataLayer || [];
-
-  function gtag() {
-    dataLayer.push(arguments);
-  }
-  gtag("js", new Date());
-  gtag("config", "G-7DJWRXDHYQ");
+  gtag('config', 'G-7DJWRXDHYQ');
+  
