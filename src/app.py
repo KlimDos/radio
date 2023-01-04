e0a1a6443a0129503961b0ea8cf84c3bd2e0dd73
Sasha Alimov <aalimov@wiley.com>
2023-01-04 14:09:45 -0500

    change message

--- src/app.py
+++ src/app.py
@@ -49,17 +49,9 @@ def radio():
 @app.route("/test")
 def test():
     app.logger.error(
-'''[2022-12-22 21:44:28,942] [productionIngestionListenerContainer-1] ERROR com.wiley.wpp.cmh.dcm.platform.packaging.research.flow.ArticlePackageService - [application::dcm-alfresco7-one][applicationEnvironment::perf][instanceId::perf][transactionId::0bfa8d4e-e67a-483f-a15e-cb4538654a29] Error creating vendor zip package
-java.lang.IllegalArgumentException: Property 'modelType' was not found
-at ccp.shared.content.jackson.ContentTypeDeserializer.lambda$findSubType$0(ContentTypeDeserializer.java:52)
-at java.base/java.util.Optional.orElseThrow(Optional.java:408)
-at ccp.shared.content.jackson.ContentTypeDeserializer.findSubType(ContentTypeDeserializer.java:52)
-at ccp.shared.content.jackson.BaseTypeDeserializer.deserializeTypedFromObject(BaseTypeDeserializer.java:38)
-at com.fasterxml.jackson.databind.deser.BeanDeserializerBase.deserializeWithType(BeanDeserializerBase.java:1292)
-at com.fasterxml.jackson.databind.deser.impl.TypeWrappedDeserializer.deserialize(TypeWrappedDeserializer.java:74)
-at com.fasterxml.jackson.databind.deser.DefaultDeserializationContext.readRootValue(DefaultDeserializationContext.java:322)
-at com.fasterxml.jackson.databind.ObjectMapper._readMapAndClose(ObjectMapper.java:4674)
-at com.fasterxml.jackson.databind.ObjectMapper.readValue(ObjectMapper.java:3682)')
+'''[test-multiline] line1
+line2
+line3)
 '''
     )
     return "check logs...", 200
