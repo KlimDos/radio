bf75523a3edab8b8f8f9cde9f809269484f35487
Sasha Alimov <klimdos@gmail.com>
2026-05-11 12:36:52 -0400

    add gtag

--- src/static/gtag.js
+++ src/static/gtag.js
@@ -4,5 +4,5 @@ window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
-  gtag('config', 'G-7DJWRXDHYQ');
+  gtag('config', 'G-VEQ7QKDT7W');
   
